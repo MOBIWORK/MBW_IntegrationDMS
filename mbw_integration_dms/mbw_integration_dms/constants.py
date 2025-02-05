4050c7b816a9a8689c2664845ae220a46aa191dd
Tuan Bui <antivenomff@gmail.com>
2025-02-05 17:04:43 +0700

    integation log

---
+++ mbw_integration_dms/mbw_integration_dms/constants.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2025, Tuanbd mbwd
+
+MODULE_NAME = "MBW Integration DMS"
+SETTING_DOCTYPE = "MBW Integration Settings"
+
+API_VERSION = "2025-01"
+
+WEBHOOK_EVENTS = [
+	"orders/create",
+	"orders/paid",
+	"orders/cancelled",
+]
+
+EVENT_MAPPER = {
+	"orders/create": "",
+	"orders/paid": "",
+	"orders/cancelled": "",
+}
+
+
+# custom fields
--- mbw_integration_dms/mbw_integration_dms/doctype/mbw_integration_log/mbw_integration_log.js
+++ mbw_integration_dms/mbw_integration_dms/doctype/mbw_integration_log/mbw_integration_log.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2025, MBW and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("MBW Integration Log", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("MBW Integration Log", {
+	refresh(frm) {
+        if (frm.doc.request_data && frm.doc.status == "Error") {
+			frm.add_custom_button(__("Retry"), function () {
+				frappe.call({
+					method: "mbw_integration_dms.mbw_integration_dms.doctype.mbw_integration_log.mbw_integration_log.resync",
+					args: {
+						method: frm.doc.method,
+						name: frm.doc.name,
+						request_data: frm.doc.request_data,
+					},
+					callback: function (r) {
+						frappe.msgprint(__("Reattempting to sync"));
+					},
+				});
+			}).addClass("btn-primary");
+		}
+	},
+});
