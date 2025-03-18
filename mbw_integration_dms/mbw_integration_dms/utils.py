895791bc71be08f02685ed690e1a34bfc4457159
Tuan Bui <antivenomff@gmail.com>
2025-03-18 16:44:26 +0700

    fix check enable

--- mbw_integration_dms/mbw_integration_dms/utils.py
+++ mbw_integration_dms/mbw_integration_dms/utils.py
@@ -15,13 +15,17 @@ def create_dms_log(**kwargs):
 	return create_log(module_def=MODULE_NAME, **kwargs)
 
 def check_enable_integration_dms():
+	enable_dms = False
 	company = frappe.defaults.get_user_default("company")
-	company_settings = frappe.get_doc(SETTING_DOCTYPE, {"name": company})
-	enable_dms = company_settings.enable_dms
+	if frappe.db.exists(SETTING_DOCTYPE, {"name": company}):
+		company_settings = frappe.get_doc(SETTING_DOCTYPE, {"name": company})
+		enable_dms = company_settings.enable_dms
 	return enable_dms
 
 def check_auto_sync_product():
+	auto_sync_product = False
 	company = frappe.defaults.get_user_default("company")
-	company_settings = frappe.get_doc(SETTING_DOCTYPE, {"name": company})
-	auto_sync_product = company_settings.auto_sync_product
+	if frappe.db.exists(SETTING_DOCTYPE, {"name": company}):
+		company_settings = frappe.get_doc(SETTING_DOCTYPE, {"name": company})
+		auto_sync_product = company_settings.auto_sync_product
 	return auto_sync_product
